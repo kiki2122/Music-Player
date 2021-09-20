d4f52eeffde802a0743a58826eaa05b509e4cce6
kiki2122 <88047418+kiki2122@users.noreply.github.com>
2021-09-20 19:38:44 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,22 @@
+let allMusic = [
+    {
+        name: "Lily",
+        artist: "Alan Walker",
+        img: "lily",
+        src: "song-lily"
+    },
+
+    {
+        name: "Lovely",
+        artist: "Billie Eilish, Khalid",
+        img: "lovely",
+        src: "song-lovely"
+    },
+
+    {
+        name: "Black Catcher",
+        artist: "Vickeblanka",
+        img: "bc",
+        src: "song-bc"
+    }
+];
